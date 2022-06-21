7f453f7a896f7ed67af88607404af1efb483aeda
Benny623 <bmilner88@gmail.com>
2022-06-20 17:12:02 -0700

    added resume page

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import pdf from '../../pdf/resume.pdf';
+
+export default function Resume() {
+    return(
+        <div className='page'>
+            <h2>Resume</h2>
+            <div id='resumeContainer'>
+                <a id='resume' href={pdf} target='_blank' rel='noreferrer'>
+                    <h5>Download PDF</h5>
+                </a>
+            </div>
+
+            <div id='proficienciesContainer'>
+                <ul>
+                    <h3>Proficiencies:</h3>
+                    <li><h5>HTML</h5></li>
+                    <li><h5>CSS</h5></li>
+                    <li><h5>JavaScript</h5></li>
+                    <li><h5>Node.js</h5></li>
+                    <li><h5>Express.js</h5></li>
+                    <li><h5>React.js</h5></li>
+                    <li><h5>SQL, NoSQL</h5></li>
+                    <li><h5>MERN Stack</h5></li>
+                    <li><h5>OOP</h5></li>
+                    <li><h5>REST</h5></li>
+                    <li><h5>APIs</h5></li>
+                    <li><h5>GraphQL</h5></li>
+                    <li><h5>MongoDB</h5></li>
+                    <li><h5>jQuery</h5></li>
+                    <li><h5>Bootstrap</h5></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
