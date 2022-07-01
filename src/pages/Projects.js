bc4086bb5566f2832bb7005c2c73561323922245
Benny623 <bmilner88@gmail.com>
2022-06-30 18:11:30 -0700

    adjusted projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -13,7 +13,9 @@ export default function Projects() {
                             <a className="btn btn-primary mt-3" href='https://github.com/BlinkingHeimdall/Regal' target='_blank' rel='noreferrer'>View on GitHub</a> 
                         </div>
                     </div>
-                    <div className='card m-5 col'>
+                </div>
+                <div className='row'>
+                <div className='card m-5 col'>
                         <div className='card-body'>
                             <a href="https://quiet-tundra-30236.herokuapp.com/" target='_blank' rel='noreferrer'><h4 className='card-header'>Budgetek</h4></a>
                             <img id='budgetek' src={require(`../images//budgetek.png`)} alt='ShowPro' className='card-img'/>
@@ -38,18 +40,16 @@ export default function Projects() {
                     </div>
                     <div className='card m-5 col'>
                         <div className='card-body'>
-                            <a href="https://bmilner88.github.io/c6-weather-dashboard/" target='_blank' rel='noreferrer'><h4 className='card-header'>Weather Dashboard</h4></a>
-                            <img id='weather-dashboard' src={require(`../images/weather-dashboard.png`)} alt='Weather Dashboard' className='card-img'/>
-                            <a className="btn btn-primary mt-3" href='https://github.com/Bmilner88/c6-weather-dashboard' target='_blank' rel='noreferrer'>View on GitHub</a> 
-                        </div>
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='card m-5 col'>
-                        <div className='card-body'>
                             <a href="https://bmilner88.github.io/c4-code-quiz/" target='_blank' rel='noreferrer'><h4 className='card-header'>Code Quiz</h4></a>
                             <img id='code=quiz' src={require(`../images/code-quiz.png`)} alt='Code Quiz' className='card-img-top'/>
                             <a className="btn btn-primary mt-3" href='https://github.com/Bmilner88/c4-code-quiz' target='_blank' rel='noreferrer'>View on GitHub</a> 
+                        </div>
+                    </div>
+                    <div className='card m-5 col'>
+                        <div className='card-body'>
+                            <a href="https://bmilner88.github.io/c6-weather-dashboard/" target='_blank' rel='noreferrer'><h4 className='card-header'>Weather Dashboard</h4></a>
+                            <img id='weather-dashboard' src={require(`../images/weather-dashboard.png`)} alt='Weather Dashboard' className='card-img'/>
+                            <a className="btn btn-primary mt-3" href='https://github.com/Bmilner88/c6-weather-dashboard' target='_blank' rel='noreferrer'>View on GitHub</a> 
                         </div>
                     </div>
                 </div>
