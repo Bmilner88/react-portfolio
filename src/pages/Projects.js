d1cc8db1137a0982470aab0e08f9a6282b6e3ea6
Benny623 <bmilner88@gmail.com>
2022-07-11 19:47:33 -0700

    fixed footer and updated resume pdf

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -30,7 +30,7 @@ export default function Projects() {
                         </div>
                     </div>
                 </div>
-                <div className='row'>
+                <div className='row bottom-cards'>
                     <div className='card m-5 col'>
                         <div className='card-body'>
                             <a href="https://bmilner88.github.io/sfcc-cg-xml-gen/" target='_blank' rel='noreferrer'><h4 className='card-header'>SFCC Customer Group XML Gen</h4></a>
@@ -38,14 +38,14 @@ export default function Projects() {
                             <a className="btn btn-primary mt-3" href='https://github.com/Bmilner88/sfcc-cg-xml-gen' target='_blank' rel='noreferrer'>View on GitHub</a> 
                         </div>
                     </div>
-                    <div className='card m-5 col'>
+                    <div className='project card m-5 col'>
                         <div className='card-body'>
                             <a href="https://bmilner88.github.io/c4-code-quiz/" target='_blank' rel='noreferrer'><h4 className='card-header'>Code Quiz</h4></a>
                             <img id='code=quiz' src={require(`../images/code-quiz.png`)} alt='Code Quiz' className='card-img-top'/>
                             <a className="btn btn-primary mt-3" href='https://github.com/Bmilner88/c4-code-quiz' target='_blank' rel='noreferrer'>View on GitHub</a> 
                         </div>
                     </div>
-                    <div className='card m-5 col'>
+                    <div className='project card m-5 col'>
                         <div className='card-body'>
                             <a href="https://bmilner88.github.io/c6-weather-dashboard/" target='_blank' rel='noreferrer'><h4 className='card-header'>Weather Dashboard</h4></a>
                             <img id='weather-dashboard' src={require(`../images/weather-dashboard.png`)} alt='Weather Dashboard' className='card-img'/>
